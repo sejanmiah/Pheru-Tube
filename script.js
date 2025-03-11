46d649f0c96f39bca7c084f9ebb38d8097a53a73
sejanmiah <sejankhan931@gmail.com>
2025-03-12 02:48:45 +0400

    Almost Done

--- script.js
+++ script.js
@@ -4,43 +4,18 @@ function loadCategories(){
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories))
 }
-// {
-//     "category_id": "1001",
-//     "category": "Music"
-// }
+
 function displayCategories(categories){
     const categoyContainer= document.getElementById("categories_container")
     for(let cat of categories){
         const categoryDiv= document.createElement("div");
         categoryDiv.innerHTML=`
-        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        <button id="btn-${cat.category_id}" onclick="loadCategoriesVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
         `;
         categoyContainer.appendChild(categoryDiv);
     }
 }
 loadCategories();
-
-// Load Vides From API
-
-
-// {
-//     "category_id": "1001",
-//     "video_id": "aaaa",
-//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-//     "title": "Shape of You",
-//     "authors": [
-//         {
-//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-//             "profile_name": "Olivia Mitchell",
-//             "verified": ""
-//         }
-//     ],
-//     "others": {
-//         "views": "100K",
-//         "posted_date": "16278"
-//     },
-//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// }
 
 
 function loadVideos(){
@@ -50,8 +25,38 @@ function loadVideos(){
 }
 
 
+const loadCategoriesVideos = (id) => {
+
+    const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+    console.log(url);
+
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        const clickedButton= document.getElementById(`btn-${id}`)
+        console.log(clickedButton)
+        clickedButton.classList.add("active")
+        displayVideos(data.category)
+    });
+}
+
+
+
 const displayVideos=(videos)=> {
     const vedioContainer= document.getElementById("vedios_container");
+
+    vedioContainer.innerHTML="";
+    if(videos.length === 0){
+        vedioContainer.innerHTML=`
+        <div class="flex col-span-full flex-col py-20 w-full text-center justify-center items-center">
+            <img src="assest/Icon.png" alt="">
+            <h2 class="text-2xl font-bold">Oops!! Sorry, There is no content here</h2>
+        </div>
+        `
+        return;
+    }
+
     videos.forEach(video=>{
         console.log(video);
         const vedioCard= document.createElement("div")
@@ -83,4 +88,8 @@ const displayVideos=(videos)=> {
     })
 };
 
-loadVideos()
+// loadVideos()
+
+
+// Load Categories Wise Videos
+
